aac8ef38e1c3b5d77ff79e6812cd79a70557e261
Kasun Rathnayake <113440627+SLBeast@users.noreply.github.com>
2023-08-02 21:47:53 +0530

    Add files via upload

---
+++ Source/js/main.js
@@ -0,0 +1,8 @@
+var header = document.getElementById("myDiv");
+    var btns = header.getElementsByClassName("navBtn");
+    for (var i = 0; i < btns.length; i++){
+        btns[i].addEventListener("click", function() {
+        var current = document.getElementsByClassName("active");
+        current[0].className = current[0].className.replace("active","");
+        this.className += " active";
+    });}
