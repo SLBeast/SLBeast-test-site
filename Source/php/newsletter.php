d84ca92463f135e61c7e4b64b838789029fb9fc3
Kasun Rathnayake <113440627+SLBeast@users.noreply.github.com>
2023-08-04 23:00:41 +0530

    Add files via upload

--- Source/php/newsletter.php
+++ Source/php/newsletter.php
@@ -10,16 +10,32 @@ if($conn->connect_error) {
 }
 else {
    
-   
-  
-    $stml = $conn->prepare("insert into newsletter(name, email) values(?,?)");
+    $stml = $conn->prepare("select email from newsletter where email = ?");
+    $stml->bind_param("s",$email);
+    $stml->execute();
+
+    $stml->store_result();
+     $stml->bind_result($column1);
+     $stml->fetch();
+
+     if(empty($column1)) {
+        $stml = $conn->prepare("insert into newsletter(name, email) values(?,?)");
     $stml->bind_param("ss",$name, $email);
     $stml->execute();
-    echo "<script>alert('You succsessfully subcribed to our newsletter'); document.location='../../index.html'</script>";
+    echo "<script>alert('You succsessfully subcribed to our newsletter'); document.location='../../index.html'</script>"; 
+        
+    }
+    else {
+        echo "<script>alert('You are already subcribed to our newsletter'); document.location='../../index.html'</script>"; 
+    }
+
+     
+  
+   
     
     $stml->close();
     $conn->close();
-
+ 
    
 }
 
